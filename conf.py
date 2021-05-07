f21fe7000b813ffe0f824d15c4fac65ddf7393a6
Haris Gušić <harisgusic.dev@gmail.com>
2021-05-07 22:49:22 +0200

    Sync with main repo + housekeeping
    Signed-off-by: Haris Gušić <harisgusic.dev@gmail.com>

--- conf.py
+++ conf.py
@@ -3,63 +3,58 @@ import os
 # Indicates whether this is built on ReadTheDocs
 READTHEDOCS = os.environ.get('READTHEDOCS', False)
 
-#########################
-# Project configuration #
-#########################
-
+# ┏━━━━━━━━━━━━━━━━━━━━━━━┓
+# ┃ Project configuration ┃
+# ┗━━━━━━━━━━━━━━━━━━━━━━━┛
 project = 'Schim'
 copyright = '2021, Haris Gušić'
 author = 'Haris Gušić'
 
-#########################
-# General configuration #
-#########################
-
+# ┏━━━━━━━━━━━━━━━━━━━━━━━┓
+# ┃ General configuration ┃
+# ┗━━━━━━━━━━━━━━━━━━━━━━━┛
 extensions = [
         'sphinx.ext.todo', 'breathe', 'sphinx.ext.autosectionlabel',
         'sphinx_tabs.tabs'
 ]
 
 # Note: the man/ directory has its own conf.py and is not built by this one
-exclude_patterns = ['_build', 'man', 'inc', 'Thumbs.db', '.DS_Store']
-todo_include_todos = True
+exclude_patterns    = ['_build', 'man', 'inc', 'Thumbs.db', '.DS_Store']
+todo_include_todos  = True
 
-##################
-# Breathe plugin #
-##################
-
-breathe_projects = { "Schim": "_build/doxygen/xml/" }
-breathe_default_project = "Schim"
+# ┏━━━━━━━━━━━━━━━━┓
+# ┃ Breathe plugin ┃
+# ┗━━━━━━━━━━━━━━━━┛
+breathe_projects                           = { "Schim": "_build/doxygen/xml/" }
+breathe_default_project                    = "Schim"
 # Stop breathe from ignoring main.cpp
 breathe_implementation_filename_extensions = []
-breathe_default_members = ('members',)
+breathe_default_members                    = ('members',)
+primary_domain                             = 'cpp'
+highlight_language                         = 'cpp'
 
-primary_domain = 'cpp'
-highlight_language = 'cpp'
+# ┏━━━━━━━━━━━━━━┓
+# ┃ HTML Options ┃
+# ┗━━━━━━━━━━━━━━┛
+html_theme      = 'sphinx_rtd_theme'
+html_favicon    = 'schim/res/img/icon.svg'
 
-################
-# HTML Options #
-################
-
-html_theme = 'sphinx_rtd_theme'
-html_favicon = 'schim/res/img/icon.svg'
-
-##########
-# Markup #
-##########
-
-smartquotes = False # Do not display '--' as long dashes
+# ┏━━━━━━━━┓
+# ┃ Markup ┃
+# ┗━━━━━━━━┛
+smartquotes = False     # Do not display '--' as long dashes
 default_role = 'envvar' # Like :code: role, but the text is black
 
 rst_prolog = ''
 # Setup snippets that will be included in all rst files
 exec(open('./include.py').read())
 
-##########################
-# Additional build steps #
-##########################
+# ┏━━━━━━━━━━━━━━━━━━━━━━━━┓
+# ┃ Additional build steps ┃
+# ┗━━━━━━━━━━━━━━━━━━━━━━━━┛
 
-# ReadTheDocs doesn't use Make -- it builds directly using sphinx
+# ReadTheDocs doesn't use Make -- it builds directly using sphinx and this file
 if READTHEDOCS:
+    # It requires some additional steps
     exec(open('./conf-rtd.py').read())
 
